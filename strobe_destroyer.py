5ccdc16ae62c3211f1f61d8ebf29d52900da54cb
Alicia Sykes <alicia.sykes@s19.tech>
2019-03-09 02:45:14 +0000

    Moved video source and output to sample dir

--- strobe_destroyer.py
+++ strobe_destroyer.py
@@ -1,9 +1,9 @@
 import cv2
-vidcap = cv2.VideoCapture('lady-gaga.mp4')
+vidcap = cv2.VideoCapture('sample-videos/lady-gaga.mp4')
 success, image = vidcap.read()
 count = 0
 while success:
-    cv2.imwrite("frame%d.jpg" % count, image)
+    cv2.imwrite("sample-output/frame%d.jpg" % count, image)
     success, image = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
