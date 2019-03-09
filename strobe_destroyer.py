2853a0a65745e4c8f3882657ec4a10379f7c0953
Ollie Poole <oliverpoole7@gmail.com>
2019-03-09 08:37:16 +0100

    Added grey output to file

--- strobe_destroyer.py
+++ strobe_destroyer.py
@@ -1,9 +1,21 @@
 import cv2
-vidcap = cv2.VideoCapture('sample-videos/lady-gaga.mp4')
-success, image = vidcap.read()
-count = 0
-while success:
-    cv2.imwrite("sample-output/frame%d.jpg" % count, image)
-    success, image = vidcap.read()
-    print('Read a new frame: ', success)
-    count += 1
+import numpy as np
+
+video = cv2.VideoCapture('lady-gaga.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+output = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
+
+while True:
+    ret, frame = video.read()
+
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    output.write(frame)
+
+    cv2.imshow('grey', grey)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+output.release()
+cv2.destroyAllWindows()
