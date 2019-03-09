74489ede3b3aa088dfe43110a05765ac3b38c35d
Ollie Poole <oliverpoole7@gmail.com>
2019-03-09 03:28:17 +0100

    removed comment

--- strobe_destroyer.py
+++ strobe_destroyer.py
@@ -3,7 +3,7 @@ vidcap = cv2.VideoCapture('lady-gaga.mp4')
 success, image = vidcap.read()
 count = 0
 while success:
-    cv2.imwrite("frame%d.jpg" % count, image)  # save frame as JPEG file
+    cv2.imwrite("frame%d.jpg" % count, image)
     success, image = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
