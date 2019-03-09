01c4e03211311f9fbca44fbde6c1b336f336fb5f
Ollie Poole <oliverpoole7@gmail.com>
2019-03-09 10:01:47 +0100

    Added support for outputing a grey frame

--- strobe_destroyer.py
+++ strobe_destroyer.py
@@ -1,19 +1,26 @@
 import cv2
 import numpy as np
 
-video = cv2.VideoCapture('lady-gaga.mp4')
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
+video = cv2.VideoCapture('video-sources/lady-gaga.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output = cv2.VideoWriter('sample-output/output.avi', fourcc, 20.0, (250, 250))
 
-while True:
+while video.isOpened():
+
     ret, frame = video.read()
 
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    output.write(frame)
+    if ret == True:
+        grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        output.write(grey)
 
-    cv2.imshow('grey', grey)
+        cv2.imshow('grey', grey)
+        print("frame saved")
+    else:
+        print("stream failed to read")
+        break
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("stream ended")
         break
 
 video.release()
